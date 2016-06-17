4d89b91550cd58b547fdcf1df154cb486da6b4c1
Rammun <russslllan@mail.ru>
2016-06-17 21:02:33 +0300

    Добавил модель регистрации

--- QuestGame.WebApi/Controllers/HomeController.cs
+++ QuestGame.WebApi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuestGame.WebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,10 @@ namespace QuestGame.WebApi.Controllers
             return View();
         }
 
-
+        public ActionResult Registration()
+        {
+            var model = new RegistrationViewModel();
+            return View(model);
+        }
     }
 }
