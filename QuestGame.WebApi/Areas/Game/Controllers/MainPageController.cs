4364d792303db44c681bd79c7c4d10c07e5966b6
Ruslan Likhobaba <likh.ruslan@gmail.com>
2016-06-30 17:29:11 +0300

    Добавил область под игру

---
+++ QuestGame.WebApi/Areas/Game/Controllers/MainPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuestGame.WebApi.Areas.Game.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: Game/MainPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
