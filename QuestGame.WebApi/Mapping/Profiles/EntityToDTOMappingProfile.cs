fea5be4430bff7fae0a4b343382fc180a7819ab3
Rammun <russslllan@mail.ru>
2016-07-20 01:38:23 +0300

    [add] Добавил новые профайлы для маппингов.

---
+++ QuestGame.WebApi/Mapping/Profiles/EntityToDTOMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuestGame.WebApi.Mapping.Profiles
+{
+    public class EntityToDTOMappingProfile : Profile
+    {
+        public EntityToDTOMappingProfile()
+        {
+
+        }
+
+        public override string ProfileName
+        {
+            get
+            {
+                return "EntityToDTOMappingProfile";
+            }
+        }
+    }
+}
