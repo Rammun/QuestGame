743596f4ffaf4e9d1823f7318819d85d75c1534c
Rammun <russslllan@mail.ru>
2016-07-22 00:54:33 +0300

    [add] Добавил инициализацию квестов при создании БД.

--- QuestGame.Domain/DBInitializers/IInitialization.cs
+++ QuestGame.Domain/DBInitializers/IInitialization.cs
@@ -12,6 +12,6 @@ namespace QuestGame.Domain.DBInitializers
 {
     public interface IInitialization
     {
-        void Initialization(ApplicationDbContext context);
+        void Initialization(ApplicationDbContext dbContext);
     }
 }
---
+++ QuestGame.Domain/DBInitializers/InitQuests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using QuestGame.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestGame.Domain.DBInitializers
+{
+    public class InitQuests : IInitialization
+    {
+        public void Initialization(ApplicationDbContext dbContext)
+        {
+            var owner = dbContext.Users.FirstOrDefault(x => x.UserName == "admin@admin.com");
+
+            for(int i = 0; i < 4; i++)
+            {
+                var quest = new Quest
+                {
+                    Title = "Title" + 1,
+                    Date = DateTime.Now,
+                    Owner = owner,
+                    Frames = new List<Frame>
+                    {
+                        new Frame { Title = "title-1 - " + i },
+                        new Frame { Title = "title-2 - " + i },
+                        new Frame { Title = "title-3 - " + i },
+                    }
+                };
+                dbContext.Quests.Add(quest);
+            }
+        }
+
+    }
+}
