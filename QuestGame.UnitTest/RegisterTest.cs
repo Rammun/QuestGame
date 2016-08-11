9d76ac411a86f678d8550aca6a4f0e640d592e40
Rammun <russslllan@mail.ru>
2016-08-11 04:34:01 +0300

    [add] Подтянута библиотека mvc в тесты. Добавлен перый контрольный тест.

---
+++ QuestGame.UnitTest/RegisterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuestGame.WebApi.Controllers;
+using System.Web.Mvc;
+
+namespace QuestGame.UnitTest
+{
+    [TestClass]
+    public class RegisterTest
+    {
+        [TestMethod]
+        public void IsViewReturn()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
