9bcf387e7ca586c6ab4660ee085bc0c9a00f4350
Rammun <russslllan@mail.ru>
2016-08-11 03:27:15 +0300

    [add] Добавил проект для юнит тестов

---
+++ QuestGame.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuestGame.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
